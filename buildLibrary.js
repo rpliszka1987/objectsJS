664022031f461a0971d09ff4a970c6cebbbbc1e5
Robert Pliszka <robplisz@gmail.com>
2022-12-27 20:33:17 -0500

    Added comments to the buildLibrary.js file

--- buildLibrary.js
+++ buildLibrary.js
@@ -1,22 +1,30 @@
+// This is a Build A Library excercise from Code Academy
+
+// Creating a Media class
 class Media {
+  // constructor for the class
   constructor(title) {
     this._title = title;
     this._isCheckedOut = false;
     this._ratings = [];
   }
 
+  //   title getter
   get title() {
     return this._title;
   }
 
+  //   rating getter
   get rating() {
     return this._ratings;
   }
 
+  //   isCheckedOut getter
   get isCheckedOut() {
     return this._isCheckedOut;
   }
 
+  //   method which changes the status from set value to opposite ( only true or false)
   toggleCheckoutStatus() {
     if (this._isCheckedOut == false) {
       this._isCheckedOut = true;
@@ -25,19 +33,22 @@ class Media {
     }
   }
 
+  //   add a rating method
   addRating(rating) {
-    this._ratings.push(rating)
+    this._ratings.push(rating);
   }
 
+  //   calculates the average of rating scores
   getAverageRating() {
-    const ratingCalculation = this._ratings.reduce((accumulator, currentValue) => accumulator + currentValue);
+    const ratingCalculation = this._ratings.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
     return ratingCalculation / this._ratings.length;
   }
 }
 
+const newItem = new Media("Testing");
+newItem.addRating(5);
+newItem.addRating(7);
 
-const newItem = new Media("Testing")
-newItem.addRating(5)
-newItem.addRating(7)
-
-console.log(newItem.getAverageRating())
+console.log(newItem.getAverageRating());
